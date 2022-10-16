24325f6c45d558a05225183cfe9fd8cf9cb1b34f
zidane-arkan <zidark897@Gmail.com>
2022-10-16 09:45:30 +0700

    Solution using Promise Chain

--- solutionUsePromises.js
+++ solutionUsePromises.js
@@ -3,18 +3,40 @@ const stocks = {
     Holder: ["Cone", "Cup", "Stick"],
     Toppings: ["Chocolate", "Sprinkles"]
 };
+let isShopOpen = true;
 
-let order = (fruitName, call_production) => {
+let order = (time, work) => {
     //Place Order
-    let findFruit = stocks.Fruits.find((fruit) => {
-        return fruit == fruitName;
+    return new Promise((resolve, reject) => {
+        if (isShopOpen) {
+            setTimeout(() => {
+                resolve(work());
+            }, time);
+        } else {
+            reject("Shop is Closed");
+        }
     });
 };
 
-let production = () => {
-    setTimeout(() => {
-        console.log("Production Ended");
-    }, 0000);
-};
-
-order("grapes", production);
+order(3000, () => `Order Ice Cream with ${stocks.Fruits[2]}`)
+    .then((result) => {
+        console.log(result);
+    })
+    .then(() => {
+        order(2000, () => console.log(`Order Ice Cream with ${stocks.Fruits[2]}`));
+    })
+    .then(() => {
+        order(1000, () => console.log(`Cut The fruit`));
+    })
+    .then(() => {
+        order(1000, () => console.log(`Add water and ice`));
+    })
+    .then(() => {
+        order(3000, () => console.log(`Start the Machine`));
+    })
+    .then(() => {
+        order(5000, () => console.log(`Serve Ice Cream`));
+    })
+    .catch((err) => {
+        console.log(err);
+    })
