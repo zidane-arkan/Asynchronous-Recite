bf9e95a011cd2e1c138b03e300b32e68beb42eef
zidane-arkan <zidark897@Gmail.com>
2022-10-16 08:32:10 +0700

    add orderInCallback

--- learnCallback.js
+++ learnCallback.js
@@ -1,39 +1,26 @@
-const fruit = {
+const stocks = {
     Fruits: ["strawberry", "grapes", "banana", "apple"],
     Holder: ["Cone", "Cup", "Stick"],
     Toppings: ["Chocolate", "Sprinkles"]
 };
 
-console.log("Order placed. Please call production");
+// console.log("Order placed. Please call production");
 
-let order = (fruitType ,holderType, ToppingsType, call_production) => { 
-    call_production();
+let order = (fruitName, call_production) => {
+    //Place Order
+    let findFruit = stocks.Fruits.find((fruit) => {
+        return fruit == fruitName;
+    });
+    setTimeout(() => {
+        console.log(`You order ${findFruit}`);
+        call_production();
+    }, 2000);
 };
 
-let production = () => { 
-    console.log("Ice Cream In Making");
+let production = () => {
+    setTimeout(() => {
+        console.log("Production Started");
+    }, 0000);
 };
 
-order(production);
-
-const step1 = () => {
-    console.log();
-};
-const step2 = () => {
-    console.log();
-};
-const step3 = () => {
-    console.log();
-};
-const step4 = () => {
-    console.log();
-};
-const step5 = () => {
-    console.log();
-};
-const step6 = () => {
-    console.log();
-};
-const step7 = () => {
-    console.log();
-};
+order("grapes", production);
